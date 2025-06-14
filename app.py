216b37b2787b6b547f5cac68b9e2e58acb285cef
AdarshOjaswiSingh <adarshojaswisingh098@gmail.com>
2025-06-14 18:36:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
-"""
-
-All code contributed to Exifa.net is © 2024 by Sahir Maharaj. 
-The content is licensed under the Creative Commons Attribution 4.0 International License. 
-This allows for sharing and adaptation, provided appropriate credit is given, and any changes made are indicated.
-
-When using the code from Exifa.net, please credit as follows: "Code sourced from Exifa.net, authored by Sahir Maharaj, 2024."
-
-For reporting bugs, requesting features, or further inquiries, please reach out to Sahir Maharaj at sahir@sahirmaharaj.com.
-
-Connect with Sahir Maharaj on LinkedIn for updates and potential collaborations: https://www.linkedin.com/in/sahir-maharaj/
-
-Hire Sahir Maharaj: https://topmate.io/sahirmaharaj/362667
-"""
-
 import streamlit as st
 import replicate
 import os
@@ -41,10 +26,7 @@ config = {
     }
 }
 
-icons = {
-    "assistant": "https://raw.githubusercontent.com/sahirmaharaj/exifa/2f685de7dffb583f2b2a89cb8ee8bc27bf5b1a40/img/assistant-done.svg",
-    "user": "https://raw.githubusercontent.com/sahirmaharaj/exifa/2f685de7dffb583f2b2a89cb8ee8bc27bf5b1a40/img/user-done.svg",
-}
+
 
 particles_js = """<!DOCTYPE html>
 <html lang="en">
